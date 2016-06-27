4b8318f2bfe7c44bcbf2d110761b02f1859f6960
Thaledric <thaledric@gmail.com>
2016-06-27 13:40:09 +0200

    Refonte interface CM, ajout des formparts

--- template/panels/cm.php
+++ template/panels/cm.php
@@ -4,14 +4,6 @@
         <input class="large" type="text" id="fcm-field-cm" name="fcm-field-cm" placeholder="Coût de mana"/>
     </div>
     <div class="input-container">
-        <div class="input-toolbar" id="fcm-cm-toolbar">
-            <button id="button-cm-manaC" class="single-inserter" data-insert="x"><img src="design/img/mana/small/X.png" alt="{x}"/></button>
-            <button id="button-cm-manaW" class="single-inserter" data-insert="w"><img src="design/img/mana/small/W.png" alt="{w}"/></button>
-            <button id="button-cm-manaU" class="single-inserter" data-insert="u"><img src="design/img/mana/small/U.png" alt="{u}"/></button>
-            <button id="button-cm-manaB" class="single-inserter" data-insert="b"><img src="design/img/mana/small/B.png" alt="{b}"/></button>
-            <button id="button-cm-manaR" class="single-inserter" data-insert="r"><img src="design/img/mana/small/R.png" alt="{r}"/></button>
-            <button id="button-cm-manaG" class="single-inserter" data-insert="g"><img src="design/img/mana/small/G.png" alt="{g}"/></button>
-            <button id="button-cm-manaS" class="single-inserter" data-insert="s"><img src="design/img/mana/small/S.png" alt="{s}"/></button>
-        </div>
+        <?php require 'template/formparts/managrid.php'; ?>
     </div>
 </section>
