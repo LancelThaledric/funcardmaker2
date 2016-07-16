f7dc6edf88873ba92964fd89c92e96f34e4a0d3d
Thaledric <thaledric@gmail.com>
2016-07-16 03:14:25 +0200

    Lien vers le forum

--- template/header.php
+++ template/header.php
@@ -5,7 +5,7 @@
         <a href="#" data-panel="template" id="fcm-new-card"><i class="fa fa-plus fa-fw"></i>&nbsp; Nouvelle carte</a>
         <a href="#" data-panel="help" id="fcm-help"><i class="fa fa-question fa-fw"></i>&nbsp; Aide</a>
         <a href="#" data-panel="tools" id="fcm-tools"><i class="fa fa-cogs fa-fw"></i>&nbsp; Outils</a>
-        <a href="#" ><i class="fa fa-comments"></i>&nbsp; Discuter</a>
+        <a href="http://smfcorp.net/forum/post.php?topic=4146" target="_blank"><i class="fa fa-comments"></i>&nbsp; Discuter</a>
         <a href="#" data-panel="credits" id="fcm-credits"><i class="fa fa-star fa-fw"></i>&nbsp; Crédits</a>
         <a href="https://bitbucket.org/Thaledric/funcardmaker2/issues?status=new&status=open" target="_blank" id="fcm-tracker"><i class="fa fa-bug fa-fw"></i>&nbsp; Bug tracker</a>
         <a href="#" data-panel="changelog" id="fcm-changelog"><i class="fa fa-file-text-o fa-fw"></i>&nbsp; Changelog</a>
