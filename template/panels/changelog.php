6eae44f859c00d945b27182ed6d1e32a2b3ba1d1
Thaledric <thaledric@gmail.com>
2016-07-19 00:45:28 +0200

    changelog affiché du plus récent au plus ancien

--- template/panels/changelog.php
+++ template/panels/changelog.php
@@ -1,18 +1,18 @@
 <section class="fcm-panel" id="fcm-panel-changelog">
     <h2>Historique des versions</h2>
     
-    <h3 class="lined">v0.1</h3>
-    <ul class="credits-list">
-        <li>Création de l'interface et du moteur de rendu</li>
-        <li>Adapatation du Template Modern Basic Real par Sovelis</li>
-    </ul>
-    
     <h3 class="lined">v0.2</h3>
     <ul class="credits-list">
         <li>Adapatation du Template 7th Edition par FirePinguinMaster</li>
         <li>Ajout des favicons</li>
         <li>Caractère spécial pour les listes à puce</li>
         <li>Fond transparent sur petits carreaux</li>
+    </ul>
+    
+    <h3 class="lined">v0.1</h3>
+    <ul class="credits-list">
+        <li>Création de l'interface et du moteur de rendu</li>
+        <li>Adapatation du Template Modern Basic Real par Sovelis</li>
     </ul>
     
     
