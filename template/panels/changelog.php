280fad3fe965f7e252f2b3c9a057b9a1a1fed7b8
Thaledric <thaledric@gmail.com>
2016-07-19 14:00:56 +0200

    v0.2.1 Changelog

--- template/panels/changelog.php
+++ template/panels/changelog.php
@@ -2,6 +2,13 @@
     <h2>Historique des versions</h2>
     
     <h3 class="lined">v0.2</h3>
+    <p>v0.2.1</p>
+    <ul class="credits-list">
+        <li>Bug : Les caractères spéciaux et les accents ne s'affichaient pas sur le template Old Basic</li>
+        <li>Bug : Le recadrage d'illustration restait affiché quand on changeait de tempate</li>
+    </ul>
+    
+    <p>v0.2.0</p>
     <ul class="credits-list">
         <li>Adapatation du Template 7th Edition par FirePinguinMaster</li>
         <li>Ajout des favicons</li>
